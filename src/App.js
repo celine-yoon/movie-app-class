e1dbcc95f497b186edb9e0befad2d6a92087fe92
Celine Yoon <celine.sy.yoon@gmail.com>
2023-04-28 12:40:00 +0900

    Add : HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div>Something...</div>;
+  return (
+    <HashRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie_summary">{summary}</p>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: { year, title, summary, poster, genres },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie_summary">{summary}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
