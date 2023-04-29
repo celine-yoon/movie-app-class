e6822eb1305f2afa97b62a0df855a33320496f77
Celine Yoon <celine.sy.yoon@gmail.com>
2023-04-30 07:02:01 +0900

    Delete: About

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
-import About from "./routes/About";
 import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
@@ -12,7 +11,6 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" exact={true} element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </HashRouter>
